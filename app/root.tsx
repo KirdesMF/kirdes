37b588b8a8f78557101126345f40ef26193238d5
KirdesGrvl <cedgourville@gmail.com>
2022-03-07 01:49:01 +0100

    🎉 First commit

---
+++ app/root.tsx
@@ -0,0 +1,43 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from 'remix';
+
+import type { MetaFunction, LinksFunction } from 'remix';
+
+import unoStyles from './styles/uno.css';
+import darkStyles from './styles/dark.css';
+
+export const meta: MetaFunction = () => {
+  return { title: 'New Remix App' };
+};
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: unoStyles },
+    { rel: 'stylesheet', href: darkStyles },
+  ];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="text-blue-600 mx-100 pt-50">
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div className="mt-8 px-8">
+      <h1>Welcome to Cedric</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
