93258342e64cc369e45e772d22e3dd222272d75e
KirdesGrvl <cedgourville@gmail.com>
2022-03-16 02:49:59 +0100

    update form contact ui

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -3,6 +3,7 @@ import type { ActionFunction } from 'remix';
 import { createContactPage } from '~/data/notion';
 
 import type { NotionContactData } from '~/data/notion';
+import { ElasticLine } from '~/components/elastic-line';
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
@@ -23,35 +24,48 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function Contact() {
   return (
-    <main className="wrapper">
-      <h1>Contactez</h1>
+    <main className="wrapper grid gap-$clamp-size-2xl py-$clamp-size-2xl">
+      <div className="flex gap-x-xs items-baseline">
+        <h1 className="text-clamp-xl font-black">
+          Get in touch <span className="text-sm">🚧</span>
+        </h1>
+        <ElasticLine />
+      </div>
+
       <Form
         reloadDocument
         method="post"
         action="/contact"
-        className="grid gap-5 color-[var(--color)]"
+        className="grid gap-y-4 color-[var(--color)] w-full md:max-w-1/2"
       >
-        <label>
-          Name: <input type="text" name="name" />
+        <label className="grid">
+          <span>Name</span>
+          <input type="text" name="name" />
         </label>
 
-        <label>
-          Last name: <input type="text" name="lastname" />
+        <label className="grid">
+          <span>Last name</span>
+          <input type="text" name="lastname" />
         </label>
 
-        <label>
-          Email: <input type="email" name="email" />
+        <label className="grid">
+          <span>Email</span>
+          <input type="email" name="email" />
         </label>
 
-        <label>
-          Subject: <input type="text" name="subject" />
+        <label className="grid">
+          <span>Subject</span>
+          <input type="text" name="subject" />
         </label>
 
-        <label>
-          Message: <textarea name="message" />
+        <label className="grid">
+          <span>Message</span>
+          <textarea name="message" />
         </label>
 
-        <button>Submit</button>
+        <button className="border-$dark-black border-2 py-4 hover:bg-$contact-lighter">
+          Submit
+        </button>
       </Form>
       <Link to="/">Home</Link>
     </main>
